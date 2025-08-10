2da01b8d3b3e48fff8f5d23555a4b32ff5097eb8
Awilka Jerome <ajeromepuente@gmail.com>
2025-08-09 23:44:04 -0400

    Estructura base del proyecto

---
+++ SistemaGestionCitas.Infrastructure/Services/Correo/Strategy/CorreoContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaGestionCitas.Infrastructure.Services.Correo.Strategy
+{
+    internal class CorreoContext
+    {
+    }
+}
