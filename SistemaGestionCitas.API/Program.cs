8a0cbc1d615110799aa2a1d633207c7cff0afe41
AlnaDesarroller <natansanchez732@gmail.com>
2025-08-14 17:24:12 -0400

    Creacion del Logger con sus respectivas Clases. Y custom del Program.cs

--- SistemaGestionCitas.API/Program.cs
+++ SistemaGestionCitas.API/Program.cs
@@ -5,10 +5,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-
 builder.Services.AddDbContext<SistemaCitasDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+// Add custom services
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Logging.AddProvider(new SistemaGestionCitas.Application.Services.LoggerProvider(
+    builder.Configuration.GetConnectionString("DefaultConnection") ?? string.Empty));
 // Add Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
