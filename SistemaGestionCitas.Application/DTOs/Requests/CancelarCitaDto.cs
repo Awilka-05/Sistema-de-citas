360c36e0b929dbe8c20e1a6055d813ac1578ed4f
Awilka Jerome <ajeromepuente@gmail.com>
2025-08-14 01:46:37 -0400

    Refactorización de DTOs en el sistema de citas

--- SistemaGestionCitas.Application/DTOs/Requests/CancelarCitaDto.cs
+++ SistemaGestionCitas.Application/DTOs/Requests/CancelarCitaDto.cs
@@ -4,16 +4,12 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using SistemaGestionCitas.Domain.Enums;
 
 namespace SistemaGestionCitas.Application.DTOs.Requests
 {
-    public class ActualizarEstadoCitaDto
+   public class CancelarCitaDto
     {
         [Required(ErrorMessage = "El id de la cita es requerido")]
         public int IdCita { get; set; }
-        [Required(ErrorMessage = "El estado es requerido")]
-        public string NuevaEstado { get; set; } = null!;
-
     }
 }
