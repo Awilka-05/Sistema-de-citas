4148d007e97880dae6c06372d5f0b7e2070e3c02
AlnaDesarroller <natansanchez732@gmail.com>
2025-08-13 21:55:47 -0400

    Cambio y Optimizacion del Launch settings, Program.cs

--- SistemaGestionCitas.API/Program.cs
+++ SistemaGestionCitas.API/Program.cs
@@ -4,26 +4,26 @@ using SistemaGestionCitas.Infrastructure.Persistence.BdContext;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
 builder.Services.AddDbContext<SistemaCitasDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddOpenApi();
+// Add Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
-                        
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
