a48eb3ba60faf35443306a075f7545ea72e5e69d
AlnaDesarroller <natansanchez732@gmail.com>
2025-08-14 23:57:18 -0400

    Corrigiendo Warnings

--- SistemaGestionCitas.Application/DTOs/Requests/LugarDto.cs
+++ SistemaGestionCitas.Application/DTOs/Requests/LugarDto.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System.ComponentModel.DataAnnotations; 
 
 namespace SistemaGestionCitas.Application.DTOs.Requests
 {
-    internal class LugarDto
+    public class LugarDto
     {
+        [Required (ErrorMessage = "El nombre del lugar es requerido")]
+        [StringLength(100, ErrorMessage = "El nombre del lugar no puede exceder los 100 caracteres")]
+        public int Nombre { get; set; }
     }
 }
