ebc7d56f596b5bd48ec0a321bbc9cce95b35abe5
Awilka Jerome <ajeromepuente@gmail.com>
2025-08-16 19:31:27 -0400

    Creación de repositorios de Servicio y Lugar

--- SistemaGestionCitas.Infrastructure/Repositories/LugarRepository.cs
+++ SistemaGestionCitas.Infrastructure/Repositories/LugarRepository.cs
@@ -3,10 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SistemaGestionCitas.Domain.Entities;
+using SistemaGestionCitas.Domain.Interfaces.Repositories;
 
 namespace SistemaGestionCitas.Infrastructure.Repositories
 {
-    internal class LugarRepository
+    public class LugarRepository : IRepository<Lugar, short>
     {
+        private readonly DbContext _context;
+
+        public LugarRepository(DbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Lugar?> GetByIdAsync(short id) =>
+            await _context.Set<Lugar>().FindAsync(id);
+
+        public async Task<IEnumerable<Lugar>> GetAllAsync() =>
+            await _context.Set<Lugar>().ToListAsync();
+
+        public async Task AddAsync(Lugar entity)
+        {
+            await _context.Set<Lugar>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Lugar entity)
+        {
+            _context.Set<Lugar>().Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(short id)
+        {
+            var entity = await GetByIdAsync(id);
+            if (entity != null)
+            {
+                _context.Set<Lugar>().Remove(entity);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<bool> ExisteNombreAsync(string nombre)
+        {
+            return await _context.Set<Lugar>().AnyAsync(l => l.Nombre == nombre);
+        }
     }
 }
